c183f845eecdb0c1a496292cf585ae451add637c
IFParicahua <itati.faviola.paricahua@gmail.com>
2021-06-26 19:54:59 -0400

    file

---
+++ app/Http/Livewire/RegistroAsignacionEquipo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class RegistroAsignacionEquipo extends Component
+{
+    public function render()
+    {
+        return view('livewire.registro-asignacion-equipo');
+    }
+}
---
+++ app/Http/Livewire/RegistroSolicitudEquipo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class RegistroSolicitudEquipo extends Component
+{
+    public function render()
+    {
+        return view('livewire.registro-solicitud-equipo');
+    }
+}
